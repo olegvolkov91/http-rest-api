44183a11b4ae40ce9c8a18556ad497e512c5220c
Oleg Volkov <voa91ua@gmail.com>
2023-01-27 21:07:04 +0200

    add user model, add migrations, add tests

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -2,6 +2,7 @@ package apiserver
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/olegvolkov91/http-rest-api/internal/app/store"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
@@ -12,6 +13,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 // New ...
@@ -30,6 +32,11 @@ func (s *APIServer) Start() error {
 	}
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
 	s.logger.Info("starting api server")
 	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
@@ -46,6 +53,16 @@ func (s *APIServer) configureLogger() error {
 
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+	return nil
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
---
+++ internal/app/store/userrepository_test.go
@@ -0,0 +1,34 @@
+package store_test
+
+import (
+	"github.com/olegvolkov91/http-rest-api/internal/app/model"
+	"github.com/olegvolkov91/http-rest-api/internal/app/store"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "user@example.org",
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	correctEmail := "user@example.org"
+	wrongEmail := "test@i.ua"
+	_, err := s.User().FindByEmail(wrongEmail)
+	assert.Error(t, err)
+
+	u, err := s.User().FindByEmail(correctEmail)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
